1cd7687b2d4281a72c0b05a42f5796ade580593f
zwcloud <zwcloud@hotmail.com>
2017-08-28 13:57:29 +0800

    Add 5.6 and 2017.1 projects.

---
+++ build/5.6.0/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: AssemblyTitle("AssetStoreTools")]
+[assembly: AssemblyDescription("unofficial improved asset store tools for Unity3D")]
+[assembly: AssemblyProduct("AssetStoreTools for Unity3D 5.5.0+")]
